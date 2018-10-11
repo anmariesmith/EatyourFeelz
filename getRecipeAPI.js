b0e3c67955c35232df94d895c366b04b5596258a
unknown <bhandari.neel@outlook.com>
2018-10-11 10:42:05 -0400

    Added JS file for Recipe API

---
+++ getRecipeAPI.js
@@ -0,0 +1,22 @@
+const getAllStock = function () {
+
+    // Need input from face++ to determine what food type to search for in Edamam
+    const foodType = '';
+    const url = 'https://api.edamam.com/search';
+
+    url += '?' + $.param({
+        'q': foodType,
+        'app_id': '3284ef04',
+        'app_key': 'f092529967b25342d06a5afe00576fdd'
+
+    });
+
+    $.ajax({
+        url: url,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+
+
+    });
+};
