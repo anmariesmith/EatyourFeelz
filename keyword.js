3e92475c80339706828b18e735969eb6725c7428
Anmarie Smith <anmariesmith007@gmail.com>
2018-10-15 11:39:00 -0400

    added additional formating for dynamically showing recipes

--- keyword.js
+++ keyword.js
@@ -9,19 +9,20 @@ const emotionsList = {
 };
 
 const keywords = {
-  anger: ["peanut", "cashew", "fruit", "kale", "berry", "apple", "green tea", "matcha", "crunchy", "egg", "whole grain", "crunchy",],
+  anger: ["peanut", "cashew", "fruit", "kale", "berry", "apple", "green tea", "matcha", "crunchy", "egg", "whole-grain", "crunchy",],
   disgust: ["soup", "garlic", "honey", "ginger", "spicy", "rice", "banana", "yogurt", "herbal", "tea", "lemon", "orange", "citrus",],
-  fear: ["dark chocolate", "salmon", "berry", "pasta", "bread", "rice", "soup", "tea", "walnut",],
+  fear: ["dark-chocolate", "salmon", "berry", "pasta", "bread", "rice", "soup", "tea", "walnut",],
   happiness: ["chewy", "tangy", "spicy", "hot", "savory", "sweet", "baked", "fried", "stir-fried", "roasated", "grilled", "creamy",],
   neutral: ["american", "chinese", "french", "greek", "italian", "indian", "japanese", "korean", "mediterranean", "mexican", "thai",],
   sadness: ["cream", "cheese", "soup", "fish", "dessert", "bread", "pasta", "comfort", "bacon", "fried", "nuts", "pizza", "cheesy"],
-  surprise: ["uni", "acai", "bonito", "fish sauce", "elver", "fatback", "jicama", "okra", "vodka", "konjac", "anchovy", "espresso", "vinegar", "pickle juice", "rose petal", "molasses", "miso", "XO sauce",],
+  surprise: ["uni", "acai", "bonito", "fish-sauce", "cherry", "steak", "potato", "okra", "vodka", "bourbon", "anchovy", "espresso", "vinegar", "pickle-juice", "rose-petal", "molasses", "miso", "XO-sauce",],
 };
 
 
-const termList =[];
+let termList =[];
 function findKeywords(emotionsList) {
   const originalList = Object.values(emotionsList);
+  termList= [];
 
   console.log(emotionsList);
 
