f75fc90f79791827bde93ac105e9c4742b799779
jjeng08 <jjeng08@users.noreply.github.com>
2018-10-19 08:54:32 -0400

    Stored js documents together, added some commentary, and removed extraneous console logs.

--- javascript/keyword.js
+++ javascript/keyword.js
@@ -1,3 +1,5 @@
+//Arrays of ingredients and food keywords that correlate to various emotions.
+//Sourced by research into the neurochemistry of emotion and nutrition.
 const keywords = {
   anger: ["peanut", "cashew", "fruit", "green", "berry", "apple", "green tea", "matcha", "crunchy", "egg", "whole-grain", "crunchy",],
   disgust: ["soup", "garlic", "honey", "ginger", "spicy", "rice", "banana", "yogurt", "herbal", "tea", "lemon", "orange", "citrus",],
@@ -11,14 +13,17 @@ const keywords = {
 let firstKey = "";
 let secondKey = "";
 
+
 function findKeywords(emotionsList) {
   const originalList = Object.values(emotionsList);
   firstKey= "";
   secondKey=""
 
+  //Breaks the emotions object returned by Face++ into two arrays.
   const listVals = Object.values(emotionsList);
   const listKeys = Object.keys(emotionsList);
 
+  //Finds the max value, stores it, splices it out, and repeats for the second highest value.
   const firstMax = Math.max(...listVals);
   const index1 = listVals.indexOf(firstMax);
   listVals.splice(index1, 1);
@@ -26,12 +31,12 @@ function findKeywords(emotionsList) {
   const secondMax = Math.max(...listVals);
   const index2 = originalList.indexOf(secondMax);
 
+  //Finds the keys based on the index of the two highest values when compared to teh original string.
   firstKey = listKeys[index1];
   secondKey = listKeys[index2];
 
+  //Posts the feelings and passes them to getRecipeAPI.js.
   const feelingsBox = $(".feelingsBox");
   feelingsBox.empty();
   feelingsBox.append(` ${firstMax}% ${firstKey} and ${secondMax}% ${secondKey}`);
-
-  console.log(`${firstKey}, ${secondKey}`);
 };
